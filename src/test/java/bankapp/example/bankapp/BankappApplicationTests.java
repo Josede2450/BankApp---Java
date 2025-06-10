e2b2fe24dc1718ed13f55b0c42df14336acf7da5
Jose De Jesus <josegabrieldejesus@hotmail.com>
2025-06-10 18:14:04 -0400

    "Temporarily disable Spring context in test to fix Jenkins CI" - 2

--- src/test/java/bankapp/example/bankapp/BankappApplicationTests.java
+++ src/test/java/bankapp/example/bankapp/BankappApplicationTests.java
@@ -1,11 +1,15 @@
 package bankapp.example.bankapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test") // <- this tells Spring to load application-test.properties
 class BankappApplicationTests {
 
 	@Test
-	void dummyTest() {
+	void contextLoads() {
 		assert true;
 	}
 }
